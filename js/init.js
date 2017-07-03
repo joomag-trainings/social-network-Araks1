7861e799e59c7050ea56c9086f4aa3d750f78b75
Araks1 <araqs.shahbazian@gmail.com>
2017-07-03 15:04:15 +0400

    social page first commit

---
+++ js/init.js
@@ -0,0 +1,19 @@
+
+function login() {
+
+    var hid=document.getElementById('login_hidden');
+    if(hid.style.display=="block"){
+        hid.style.display="none";
+    }
+    else{
+        hid.style.display="block";
+    }
+
+
+
+}
+/*function modal() {
+var modal=document.getElementById('modal');
+modal.style.display="block";
+
+}*/
